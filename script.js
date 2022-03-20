945fb202ab43bba27353058a2c786df2bc5461a0
akhang <lnak211099@gmail.com>
2022-03-20 20:26:15 +0700

    Make computerPlay randomly return and print R/P/S

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+let computerSelection;
 function computerPlay() {
     let x = Math.floor(Math.random() * 3) + 1;
-    return x;    
+    if (x === 1) {
+        computerSelection = "Rock";
+    } else if (x === 2) {
+        computerSelection = "Paper";
+    } else
+        computerSelection = "Scissors";
+    return computerSelection;
 }
 
-console.log(computerPlay())
+console.log(computerPlay());
