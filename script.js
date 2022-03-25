0784ffab64c93b82122b8b737c0ca3e33672b4f3
akhang <lnak211099@gmail.com>
2022-03-25 16:32:49 +0700

    (Multi commits below) Add game() function call Fix declare computerSelection and playerSelection. Fix the return of playRound. Change the first parameter to computerSelection = computerPlay() in the for loop. Add console.log() the final result

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const computerSelection = computerPlay();
-const playerSelection = "Rock";
+let computerSelection;
+let playerSelection;
 
 function computerPlay() {
     let x = Math.floor(Math.random() * 3) + 1;
@@ -14,27 +14,48 @@ function computerPlay() {
 function playRound(computerSelection, playerSelection) {
     playerSelection = prompt("What do you choose between Rock / Paper / Scissors?");
     let playerS = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+    let roundResult,
+        roundResultMessage;
     if (computerSelection === playerS) {
-        return "This is a draw match.";
+        roundResult = 1;
+        roundResultMessage = "This is a draw match.";
     } else if ((computerSelection === "Rock" && playerS === "Paper") || 
                (computerSelection === "Paper" && playerS === "Scissors") || 
                (computerSelection === "Scissors" && playerS === "Rock")) {
-        return `You win! ${playerS} beat(s) ${computerSelection}.`;
+        roundResult = 2;
+        roundResultMessage = `You win! ${playerS} beat(s) ${computerSelection}.`;
     } else {
-        return `You lose! ${playerS} lose(s) to ${computerSelection}.`;
+        roundResult = 0;
+        roundResultMessage = `You lose! ${playerS} lose(s) to ${computerSelection}.`;
     }
+    return [roundResult, roundResultMessage];
 }
 
 function game() {
     let computerScore = 0;
     let playerScore = 0;
-    let roundResult;
+    let result1;
     for (let i = 0; i < 5; i++) {
-        roundResult = playRound();
-        if (roundResult === 1) {
+        result1 = playRound(computerSelection = computerPlay(), playerSelection);
+        const roundResult = result1[0],
+              roundResultMessage = result1[1];
+        if (roundResult === 0) {
             computerScore++;
+            console.log(roundResultMessage);
         } else if (roundResult === 2) {
             playerScore++;
+            console.log(roundResultMessage);
+        } else {
+            console.log(roundResultMessage);
         }
     }
+    if (computerScore < playerScore) {
+        console.log(`Final score: Computer ${computerScore} : ${playerScore} You\nCongratulation! You win the game!`);
+    } else if (computerScore > playerScore) {
+        console.log(`Final score: Computer ${computerScore} : ${playerScore} You\nOh no! You lose...`);
+    } else {
+        console.log(`Final score: Computer ${computerScore} : ${playerScore} You\nThe game ended in a draw.`);
+    }
 }
+
+game();
